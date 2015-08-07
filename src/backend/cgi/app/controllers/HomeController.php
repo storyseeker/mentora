6aa7f88b3400071cf505bf0edc1358a1bc6d2c68
linzhengwei <linzhengwei@xiaomi.com>
2015-08-07 20:17:49 +0800

    phlacon partial views

--- src/backend/cgi/app/controllers/HomeController.php
+++ src/backend/cgi/app/controllers/HomeController.php
@@ -4,8 +4,11 @@ use Phalcon\Mvc\Controller;
 
 class HomeController extends Controller
 {
-
     public function indexAction()
     {
+        $this->view->setVars(array(
+            'title' => 'David',
+            'content' => 'Crown'
+        ));
     }
 }
