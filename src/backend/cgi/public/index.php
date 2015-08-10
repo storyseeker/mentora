9ba007844289c02e6f7f0fdd893fed4cd54a5024
linzhengwei <linzhengwei@xiaomi.com>
2015-08-10 10:50:28 +0800

    config.ini

--- src/backend/cgi/public/index.php
+++ src/backend/cgi/public/index.php
@@ -7,15 +7,22 @@ use Phalcon\Mvc\View;
 use Phalcon\Mvc\Application;
 use Phalcon\DI\FactoryDefault;
 use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+use Phalcon\Config\Adapter\Ini as ConfigIni;
+
+define('APP_PATH', realpath('..') . '/');
+define('CONFIG_PATH', APP_PATH . 'app/config/config.ini');
+define('COMPILED_VIEW_PATH', APP_PATH. 'app/compiled-views/');
 
 try {
+    // Read the configuration
+    $config = new ConfigIni(CONFIG_PATH);
+
     // Register an autoloader
     $loader = new Loader();
     $loader->registerDirs(
         array(
-            '../app/controllers/',
-            '../app/models/',
-            '../app/libs/'
+            APP_PATH . $config->application->controllersDir,
+            APP_PATH . $config->application->modelsDir,
         )
     )->register();
 
@@ -40,7 +47,7 @@ try {
         $volt = new \Phalcon\Mvc\View\Engine\Volt($view, $di);
 
         $volt->setOptions(array(
-            "compiledPath" => "../app/compiled-views/",
+            "compiledPath" => COMPILED_VIEW_PATH,
             "compiledExtension" => ".php"
         ));
 
@@ -65,7 +72,7 @@ try {
     // Setup a base URI so that all generated URIs include the "tutorial" folder
     $di['url'] = function() {
         $url = new Url();
-        $url->setBaseUri('/cgi/');
+        $url->setBaseUri($config->application->baseUri);
         return $url;
     };
 
